b93b33797e3b2be25f4cb9949d17775cbb97914e
botrott <padgetalex@yandex.ru>
2023-12-25 20:14:37 +0300

    add menu, lexicon

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from aiogram.utils import executor
-from config.config import dp
+from config.config import dp, bot
 from handlers import client, admin
 from logibot.loggerbot import logger
-
+from keyboards_inline.menu import set_main_menu
 
 async def on_startup(_):
     logger.info('Start BOT')
+    await set_main_menu(bot)
     print('Start')
 
 
